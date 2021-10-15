9945565b2d197b30c94157499e65f220636a40d0
Bipsa0007 <91912361+Bipsa0007@users.noreply.github.com>
2021-10-15 23:21:01 +0530

    Create binary_exponentiation.cpp
    binary exponentiation in cpp

---
+++ binary_exponentiation.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+ll bin_exp(ll a, ll n)
+{
+    ll val=1;
+    while(n)
+    {
+        if (n%2)
+        {
+            val*=a;
+        }
+        a*=a;
+        n=n/2;
+    }
+    return val;
+}
+
+int main()
+{
+    ll a,n;
+    cin>>a>>n;
+    cout<<bin_exp(a,n);
+}
